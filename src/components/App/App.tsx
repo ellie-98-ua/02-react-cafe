02d449ad2ce0fc7c0db74236df37dcb93527beba
Elvira Antoniuk <elviraantonuk5@gmail.com>
2025-09-01 19:25:03 +0100

    folders

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import css from './App.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
-import VoteOptions from '../VoteOptions';
-import VoteStats from '../VoteStats';
-import Notification from '../Notification';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 import type { Votes, VoteType } from '../../types/votes';
 
 export default function App() {
