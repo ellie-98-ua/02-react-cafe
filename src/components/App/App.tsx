03fd1194a4b63b1ee657223b02f0fbd375498db3
Elvira Antoniuk <elviraantonuk5@gmail.com>
2025-09-01 19:21:24 +0100

    с

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import css from './App.module.css';
-import CafeInfo from '../CafeInfo';
+import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions';
 import VoteStats from '../VoteStats';
 import Notification from '../Notification';
